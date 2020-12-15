f5b8ba77f40186f5408ad126f762ac2dfcc287e1
MissNG <65439386+MissNG-Git@users.noreply.github.com>
2020-12-16 01:17:39 +1100

    pseudocode js

---
+++ Dev/script.js
@@ -0,0 +1,25 @@
+// Display today's date & time
+
+$(document).ready(function() {
+  // On click event listener
+
+  // Save textarea input to local storage
+
+  // Function to register hour & change background colors accordingly
+
+    // Get current # of hours
+
+    // Loop over time blocks & check time
+
+      // **use classes: future, past & present (?)**
+      // If var1 less than var2 ...
+      
+      // Else if var1 === var2 ...
+
+      // Else ...
+    
+  // Call function
+
+  // Display key: values from local storage
+
+  });
