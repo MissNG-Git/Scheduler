284a981ee6ccae4ba9a40b8445806809abf67d18
MissNG <65439386+MissNG-Git@users.noreply.github.com>
2020-12-16 17:14:39 +1100

    set eventListener & color fxn

--- Dev/script.js
+++ Dev/script.js
@@ -12,33 +12,35 @@ setInterval(updateTime, 1000);
 
 $(document).ready(function() {
   // On click event listener
-
-
+  $(".saveBtn").on("click", function () {
+    // Get value of description class entered at relevant time
+    var time = $(this).parent().attr("id");
+    var text = $(this).siblings(".description").val();
+    
   // Save textarea input to local storage
-
+  localStorage.setItem(time, text);
+})
 
   // Function to register hour & change background colors accordingly
-
-
+  function timeTracker() {
     // Get current # of hours
-
-
+    var nowHour = moment().hour();
     // Loop over time blocks & check time
-
-
+    $("#time-block").each(function() {
+      var blockHour = 
       // **use classes: future, past & present (?)**
       // If var1 less than var2 ...
       
-
       // Else if var1 === var2 ...
-
-
+      
       // Else ...
-    
+      
+    };
+  }
 
   // Call function
+  timeTracker ();
 
-  
   // Display key: values from local storage
 
-  });
+});
