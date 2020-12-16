04b5e7531fabe38d66daed0443e9eba5d054ff5b
MissNG <65439386+MissNG-Git@users.noreply.github.com>
2020-12-17 00:44:58 +1100

    display stored data to scheduler

--- Dev/script.js
+++ Dev/script.js
@@ -1,17 +1,16 @@
-// Display today's date & time
+// Display today's date
 var nowDate = moment().format('LL');
 $("#currentDay").html(nowDate);
 
-// Make moment.js time dynamic
+// Display today's time and make dynamic
 function updateTime() {
   var nowTime = moment().format('LTS');
   $("#currentTime").html(nowTime);
 }
-updateTime();
 setInterval(updateTime, 1000);
 
 $(document).ready(function() {
-  // On click event listener
+  // On click event listener for button
   $(".saveBtn").on("click", function () {
     // Get value of description class entered at relevant time
     var time = $(this).parent().attr("id");
@@ -48,6 +47,15 @@ $(document).ready(function() {
     }
   });
 
-  // Display key: values from local storage
-
+  // Display key: values from local storage onto schedule
+  $("#9AM .description").val(localStorage.getItem("9AM"));
+  $("#10AM .description").val(localStorage.getItem("10AM"));
+  $("#11AM .description").val(localStorage.getItem("11AM"));
+  $("#12PM .description").val(localStorage.getItem("12PM"));
+  $("#1PM .description").val(localStorage.getItem("1PM"));
+  $("#2PM .description").val(localStorage.getItem("2PM"));
+  $("#3PM .description").val(localStorage.getItem("3PM"));
+  $("#4PM .description").val(localStorage.getItem("4PM"));
+  $("#5PM .description").val(localStorage.getItem("5PM"));
+  $("#6PM .description").val(localStorage.getItem("6PM"));
 });
