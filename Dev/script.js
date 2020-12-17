8be3b3248d7eb5d521c2da420755b14e420d082b
MissNG <65439386+MissNG-Git@users.noreply.github.com>
2020-12-17 16:17:09 +1100

    'dry' code value display

--- Dev/script.js
+++ Dev/script.js
@@ -48,14 +48,9 @@ $(document).ready(function() {
   });
 
   // Display key: values from local storage onto schedule
-  $("#9AM .description").val(localStorage.getItem("9AM"));
-  $("#10AM .description").val(localStorage.getItem("10AM"));
-  $("#11AM .description").val(localStorage.getItem("11AM"));
-  $("#12PM .description").val(localStorage.getItem("12PM"));
-  $("#1PM .description").val(localStorage.getItem("1PM"));
-  $("#2PM .description").val(localStorage.getItem("2PM"));
-  $("#3PM .description").val(localStorage.getItem("3PM"));
-  $("#4PM .description").val(localStorage.getItem("4PM"));
-  $("#5PM .description").val(localStorage.getItem("5PM"));
-  $("#6PM .description").val(localStorage.getItem("6PM"));
+  var timeArray = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM", "6PM"]
+  for (var i =0; i < timeArray.length; i++) {
+    $("#"+timeArray[i] + " .description").val(localStorage.getItem(timeArray[i]));
+  }
+
 });
